0d6d9a17e366947a6ba22659e501fd97def3e307
Mason <masonfernandez@outlook.com>
2025-01-27 00:46:51 -0700

    installed skeleton and lucide

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script lang="ts">
+    import { ProgressRing } from '@skeletonlabs/skeleton-svelte';
+  </script>
+  
+
+  <div class="grid grid-cols-1 gap-4 justify-center">
+    <ProgressRing value={25} max={100} showLabel />
+  </div>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-pink-900">hihihihihihih</h1>
