5d4b63d55b75a5a6cfa424c4584fed29a8613ddf
Mason <masonfernandez@outlook.com>
2025-01-27 20:13:25 -0700

    initialize weaviate connection

---
+++ src/routes/check_readiness/+page.server.ts
@@ -0,0 +1,32 @@
+import weaviate, { connectToWeaviateCloud } from 'weaviate-client'
+import type { WeaviateClient } from 'weaviate-client'
+import type { PageServerLoad } from '/$types'
+
+let client: WeaviateClient
+
+//thorteks thingy for custom port
+async function connectToWeaviate() : Promise <WeaviateClient> {
+    const clientPromise = weaviate.connectToCustom({
+        httpHost: 'localhost',
+        httpPort: 3700,
+        grcpHost: 'localhost',
+        grcpPort: 50054
+    });
+    return clientPromise;
+}
+
+export const load: PageServerLoad = async () => {
+    //thors thingy for custom port
+    //client = await connectToWeaviate()
+    client = await weaviate.connectToLocal()
+
+    const clientReadiness = await client.isReady()
+
+    console.log('Client is ready', clientReadiness )
+
+    client.close()
+
+    return {
+        ready: clientReadiness
+    }
+}
---
+++ src/routes/check_readiness/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { PageData } from '/$types'
+
+    let { data }: { data: PageData } = $props()
+    console.log(data)
+</script>
+
+<h1>Check readiness</h1>
+<p>Is Weaviate Ready?: { data.ready ? 'yes' : 'no'}</p>
