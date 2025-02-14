2ab9bd1b8710fe6dcb736965c05de7f07dd4ab3a
Mason <masonfernandez@outlook.com>
2025-02-14 12:34:54 -0700

    basic navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
 	import '../app.css';
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+  	import CircleUser from 'lucide-svelte/icons/circle-user';
+  	import Github from 'lucide-svelte/icons/github';
 	let { children } = $props();
 </script>
+
+<nav>
+	<AppBar>
+		{#snippet lead()}
+		  <CircleUser size={32} />
+		  <span>370 AI App</span>
+		  <div class="flex gap-2">
+			<a href="/"><button type="button" class="btn preset-filled-primary-500">Home</button></a>
+			<a href="/chat"><button type="button" class="btn preset-tonal-primary">Chatbot</button></a>
+			<a href="/check_readiness"><button type="button" class="btn preset-outlined-primary-500">Weaviate</button></a>
+		  </div>
+		{/snippet}
+		{#snippet trail()}
+		  <Github size={20} />
+		{/snippet}
+	  </AppBar>
+</nav>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,3 @@
 <script lang="ts">
-  import { AppBar } from '@skeletonlabs/skeleton-svelte';
-  import ArrowLeft from 'lucide-svelte/icons/arrow-left';
-  import Paperclip from 'lucide-svelte/icons/paperclip';
-  import Calendar from 'lucide-svelte/icons/calendar';
-  import CircleUser from 'lucide-svelte/icons/circle-user';
-  import Github from 'lucide-svelte/icons/github';
+
 </script>
-  
-<AppBar>
-  {#snippet lead()}
-    <CircleUser size={32} />
-    <span>370 AI App</span>
-    <div class="flex gap-2">
-      <button type="button" class="btn preset-filled-primary-500">Home</button>
-      <button type="button" class="btn preset-tonal-primary">Chatbot</button>
-      <button type="button" class="btn preset-outlined-primary-500">Weaviate</button>
-    </div>
-  {/snippet}
-  {#snippet trail()}
-    <Github size={20} />
-  {/snippet}
-</AppBar>
